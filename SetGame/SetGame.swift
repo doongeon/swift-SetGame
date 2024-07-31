24779b46963987c8958a0afb10001dcea890ccd5
donggeon Na <ndmb2012@gmail.com>
2024-07-31 13:54:06 +0900

    Model of Set game

---
+++ SetGame/SetGame.swift
@@ -0,0 +1,45 @@
+//
+//  SetGameCard.swift
+//  SetGame
+//
+//  Created by 나동건 on 7/31/24.
+//
+
+import Foundation
+
+struct SetGame<CardContent> {
+    private(set) var deck: Array<Card>
+    private(set) var choices: Array<Card>
+    
+    init(_ deckGenerator: () -> Array<Card>) {
+        self.deck = deckGenerator()
+        self.choices = []
+        
+        for _ in 0..<12 {
+            if let popedCard = deck.popLast() {
+                choices.append(popedCard)
+            }
+        }
+    }
+    
+    mutating func draw() -> Void {
+        for _ in 0..<3 {
+            if let popedCard = deck.popLast() {
+                choices.append(popedCard)
+            }
+        }
+    }
+    
+    func choose(_ card: Card) -> Void {
+        print(card)
+    }
+    
+    struct Card: Identifiable {
+        var color: String
+        var count: Int
+        var content: CardContent
+        var shade: String
+        
+        var id: String
+    }
+}
